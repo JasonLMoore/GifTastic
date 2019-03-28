d1f3d5a6f364917c6e7b1daa7f3051fd5a3374b6
Jason Moore <jason.moore1369@gmail.com>
2019-03-27 19:11:29 -0500

    added some html and java

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,11 @@
+var topics = ["Kansas Jayhawks", "NBA", "College Basketball", "Referees", "Insane Fans", "Monster Dunks", "Mascots"];
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=shNxgbRQV8gE0hvyjnnSdqG6le4hI2jT";
+
+function renderButtons() {
+    $("#buttonsPlace").empty();
+    
+    for (b=0; b < topics.length; b++) {
+        var bttn = $("<button>");
+        b.addClass()
+    }
+}
