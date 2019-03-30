289eafc624bbfee3e8e954d524f64bbf4a4b6c99
Jason Moore <jason.moore1369@gmail.com>
2019-03-30 15:40:55 -0500

    added bootstrap styling and buttons generate and populate

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -2,10 +2,25 @@ var topics = ["Kansas Jayhawks", "NBA", "College Basketball", "Referees", "Insan
 var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=shNxgbRQV8gE0hvyjnnSdqG6le4hI2jT";
 
 function renderButtons() {
-    $("#buttonsPlace").empty();
+    $("#buttons-view").empty();
     
     for (b=0; b < topics.length; b++) {
         var bttn = $("<button>");
-        b.addClass()
+        bttn.addClass("topic");
+        bttn.attr("data-name", topics[b]);
+        bttn.text(topics[b]);
+        $("#buttons-view").append(bttn);
     }
+
 }
+
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
+    var topic = $("#gif-input").val().trim();
+    topics.push(topic);
+    console.log(topics);
+    renderButtons();
+    
+});
+renderButtons();
+console.log(topics)
