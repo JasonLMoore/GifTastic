6f0cea5d5d85e9894b4bb81ce27bb06f7439c8d2
Jason Moore <jason.moore1369@gmail.com>
2019-03-31 13:41:43 -0500

    original array populating gif's, after adding new button no longer works

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,5 +1,5 @@
 var topics = ["Kansas Jayhawks", "NBA", "College Basketball", "Referees", "Insane Fans", "Monster Dunks", "Mascots"];
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=shNxgbRQV8gE0hvyjnnSdqG6le4hI2jT";
+
 
 function renderButtons() {
     $("#buttons-view").empty();
@@ -7,20 +7,50 @@ function renderButtons() {
     for (b=0; b < topics.length; b++) {
         var bttn = $("<button>");
         bttn.addClass("topic");
-        bttn.attr("data-name", topics[b]);
+        bttn.attr("data-topic", topics[b]);
         bttn.text(topics[b]);
         $("#buttons-view").append(bttn);
     }
+    
+};
+renderButtons();
 
-}
 
 $("#add-gif").on("click", function(event) {
     event.preventDefault();
     var topic = $("#gif-input").val().trim();
     topics.push(topic);
-    console.log(topics);
     renderButtons();
+    console.log(topics)
+});
+
+$("button").on("click", function() {
+    var topic = $(this).attr("data-topic");
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=shNxgbRQV8gE0hvyjnnSdqG6le4hI2jT&q=" + topic + "&limit=10";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }) .then(function(response) {
+        console.log(queryURL);
+        console.log(response);
+        var results = response.data;
+        
+        for (var i = 0; i < results.length; i++) {
+            var topicDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            
+            var topicGif = $("<img>");
+            topicGif.attr("src", results[i].images.fixed_height.url);
+            
+            topicDiv.append(p);
+            topicDiv.append(topicGif);
+            $("#gif-container").prepend(topicDiv);
+        }
+    });
+    
+    
+    
     
 });
-renderButtons();
-console.log(topics)
